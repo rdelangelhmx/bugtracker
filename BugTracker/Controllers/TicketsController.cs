88b567914b9aee97bc625208f985e5056c480346
Adam Eury <adameury@outlook.com>
2014-07-16 09:36:58 -0400

    First commit

---
+++ BugTracker/Controllers/TicketsController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BugTracker.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BugTracker.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public TicketsController() { }
+
+        private BugTrackerEntities1 db = new BugTrackerEntities1();
+        private ApplicationDbContext Db = new ApplicationDbContext();
+        private UserManager<ApplicationUser> UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            var tickets = db.Tickets.Include(t => t.Project).Include(t => t.TicketPriority).Include(t => t.TicketStatus).Include(t => t.TicketType);
+
+            List<ListTicketsViewModel> model = new List<ListTicketsViewModel>();
+            foreach (var item in tickets)
+            {
+                model.Add(new ListTicketsViewModel(item));
+            }
+            return View(model);
+        }
+
+        // GET: Tickets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Create
+        public ActionResult Create()
+        {
+            var model = new CreateTicketViewModel();
+
+            //model.Assignees = new List<SelectListItem>();
+            //foreach (var item in Db.Users)
+            //{
+            //    model.Assignees.Add(new SelectListItem() { Value = item.Id, Text = item.UserName });
+            //}
+            model.Assignees = new SelectList(Db.Users, "Id", "UserName");
+
+            //model.Projects = new List<SelectListItem>();
+            //foreach (var item in db.Projects)
+            //{
+            //    model.Projects.Add(new SelectListItem() { Value = item.ID.ToString(), Text = item.Name });
+            //}
+            model.Projects = new SelectList(db.Projects, "ID", "Name");
+
+            //model.Priorities = new List<SelectListItem>();
+            //foreach (var item in db.TicketPriorities)
+            //{
+            //    model.Priorities.Add(new SelectListItem() { Value = item.ID.ToString(), Text = item.Name });
+            //}
+            model.Priorities = new SelectList(db.TicketPriorities, "ID", "Name");
+
+            //model.Statuses = new List<SelectListItem>();
+            //foreach (var item in db.TicketStatuses)
+            //{
+            //    model.Statuses.Add(new SelectListItem() { Value = item.ID.ToString(), Text = item.Name });
+            //}
+            model.Statuses = new SelectList(db.TicketStatuses, "ID", "Name");
+
+            //model.Types = new List<SelectListItem>();
+            //foreach (var item in db.TicketTypes)
+            //{
+            //    model.Types.Add(new SelectListItem() { Value = item.ID.ToString(), Text = item.Name });
+            //}
+            model.Types = new SelectList(db.TicketTypes, "ID", "Name");
+
+            return View(model);
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateTicketViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //ticket.SubmitterID = User.Identity.GetUserId();
+                //ticket.AssigneeID = UserManager.FindByEmail(ticket.AssigneeID).Id;
+                //ticket.Created = System.DateTime.Now;
+                //db.Tickets.Add(ticket);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            //ViewBag.ProjectID = new SelectList(db.Projects, "ID", "Name", ticket.ProjectID);
+            //ViewBag.PriorityID = new SelectList(db.TicketPriorities, "ID", "Name", ticket.PriorityID);
+            //ViewBag.StatusID = new SelectList(db.TicketStatuses, "ID", "Name", ticket.StatusID);
+            //ViewBag.TypeID = new SelectList(db.TicketTypes, "ID", "Name", ticket.TypeID);
+            //return View(ticket);
+            return View(model);
+        }
+
+        // GET: Tickets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ID", "Name", ticket.ProjectID);
+            ViewBag.PriorityID = new SelectList(db.TicketPriorities, "ID", "Name", ticket.PriorityID);
+            ViewBag.StatusID = new SelectList(db.TicketStatuses, "ID", "Name", ticket.StatusID);
+            ViewBag.TypeID = new SelectList(db.TicketTypes, "ID", "Name", ticket.TypeID);
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserID,Title,Description,Created,Updated,AssignedToUserID,ProjectID,TypeID,PriorityID,StatusID")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProjectID = new SelectList(db.Projects, "ID", "Name", ticket.ProjectID);
+            ViewBag.PriorityID = new SelectList(db.TicketPriorities, "ID", "Name", ticket.PriorityID);
+            ViewBag.StatusID = new SelectList(db.TicketStatuses, "ID", "Name", ticket.StatusID);
+            ViewBag.TypeID = new SelectList(db.TicketTypes, "ID", "Name", ticket.TypeID);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ticket ticket = db.Tickets.Find(id);
+            db.Tickets.Remove(ticket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BugTracker/Models/TicketViewModels.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+
+namespace BugTracker.Models
+{
+    public class ListTicketsViewModel
+    {
+        public ListTicketsViewModel() { }
+
+        public ListTicketsViewModel(Ticket ticket) : this()
+        {
+            var db = new ApplicationDbContext();
+
+            this.ID = ticket.ID;
+            var submitter = db.Users.First(m => m.Id == ticket.SubmitterID);
+            this.Submitter = submitter.UserName;
+            this.Title = ticket.Title;
+            this.Description = ticket.Description;
+            this.Created = ticket.Created.ToString();
+            this.Updated = ticket.Updated.ToString();
+
+            var assignee = db.Users.First(m => m.Id == ticket.AssigneeID);
+            this.Assignee = (assignee != null) ? assignee.UserName : "Unassigned";
+
+            this.Project = ticket.Project.Name;
+            this.Priority = ticket.TicketPriority.Name;
+            this.Status = ticket.TicketStatus.Name;
+            this.Type = ticket.TicketType.Name;
+        }
+
+        public int ID { get; set; }
+
+        [Display(Name = "Submitter")]
+        public string Submitter { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Created")]
+        public string Created { get; set; }
+
+        [Display(Name = "Updated")]
+        public string Updated { get; set; }
+
+        [Display(Name = "Assignee")]
+        public string Assignee { get; set; }
+
+        [Display(Name = "Project")]
+        public string Project { get; set; }
+
+        [Display(Name = "Priority")]
+        public string Priority { get; set; }
+
+        [Display(Name = "Status")]
+        public string Status { get; set; }
+
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+    }
+
+    public class CreateTicketViewModel
+    {
+        public CreateTicketViewModel() { }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Assignee")]
+        //public List<SelectListItem> Assignees { get; set; }
+        public SelectList Assignees { get; set; }
+        public string Assignee { get; set; }
+
+        [Display(Name = "Project")]
+        //public List<SelectListItem> Projects { get; set; }
+        public SelectList Projects { get; set; }
+        public string Project { get; set; }
+
+        [Display(Name = "Priority")]
+        //public List<SelectListItem> Priorities { get; set; }
+        public SelectList Priorities { get; set; }
+        public string Priority { get; set; }
+
+        [Display(Name = "Status")]
+        //public List<SelectListItem> Statuses { get; set; }
+        public SelectList Statuses { get; set; }
+        public string Status { get; set; }
+
+
+        [Display(Name = "Type")]
+        //public List<SelectListItem> Types { get; set; }
+        public SelectList Types { get; set; }
+        public string Type { get; set; }
+    }
+}
